28c2cbbcb92ccf11e7131c073a5112e474360160
Willard Shikami <willardshikami@gmail.com>
2020-09-28 22:40:49 +0300

    feat: add beneficiary page

--- lib/ui/addbeneficiary.dart
+++ lib/ui/addbeneficiary.dart
@@ -7,12 +7,100 @@ class AddBeneficiary extends StatelessWidget {
         child: Scaffold(
       body: SafeArea(
         child: Container(
-          child: Row(
+          child: Column(
             children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: null,
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.chevron_left,
+                      color: Colors.black,
+                      size: 32,
+                    ),
+                    onPressed: null,
+                  ),
+                  IconButton(
+                      icon: Icon(
+                        Icons.more_vert,
+                        color: Colors.black,
+                        size: 32,
+                      ),
+                      onPressed: null)
+                ],
+              ),
+              SizedBox(
+                height: 26,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Adding a beneficiary',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 24,
+                      left: 24,
+                    ),
+                    child: Text(
+                      'Your beneficiary receives your retirement assets in the even of your death',
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  height: 150,
+                  width: 350,
+                  color: Colors.green[50],
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundImage: AssetImage('assets/avatar.png'),
+                        backgroundColor: Colors.green[100],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(24),
+                        child: Container(
+                          height: 40,
+                          width: 200,
+                          color: Colors.green[100],
+                          child: Center(
+                            child: Text(
+                              'Add a trusted contact',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
